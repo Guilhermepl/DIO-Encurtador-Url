846a9fa1911aabc2625779d5c32b1620e1fdbbd6
Guilhermepl <32499476+Guilhermepl@users.noreply.github.com>
2022-02-25 16:15:26 -0300

    Add files via upload

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://guilhermepl:<159!%(>@url-shortener-dio.rksne.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
